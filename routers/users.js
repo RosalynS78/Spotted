6546625f6841155e3fec00f1fc657378a5f93616
RosalynS78 <rrcervania78@gmail.com>
2022-05-22 22:11:19 -0500

    guh

--- routers/users.js
+++ routers/users.js
@@ -2,22 +2,22 @@ const express = require('express')
 const usersController = require('../controllers/users')
 const router = express.Router()
 
-router.get('/users', usersController.getAllUsers)
+router.get('/', usersController.getAllUsers)
 
-router.get('/users:id', usersController.getUserById)
+router.get('/:id', usersController.getUserById)
 
-router.post('/users', usersController.createUser)
+router.post('/', usersController.createUser)
 
-router.post('/users', usersController.createUserContact)
+router.post('/', usersController.createUserContact)
 
-router.post('/users', usersController.createUserLocation)
+router.post('/', usersController.createUserLocation)
 
-router.put('/users:id', usersController.updateUserById)
+router.put('/:id', usersController.updateUserById)
 
-router.put('/users:id', usersController.updateUserContact) 
+router.put('/:id', usersController.updateUserContact) 
 
-router.put('/users:id', usersController.updateUserLocation)
+router.put('/:id', usersController.updateUserLocation)
 
-router.delete('/users:id', usersController.deleteUserById)
+router.delete('/:id', usersController.deleteUserById)
 
 module.exports = router
