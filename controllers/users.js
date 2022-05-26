c108a31fe831f9597ddb99d554320631e2cc8491
RosalynS78 <rrcervania78@gmail.com>
2022-05-26 00:12:22 -0500

    users working

--- controllers/users.js
+++ controllers/users.js
@@ -18,30 +18,11 @@ const getUserById = (req, res) => {
     return res.json(rows);
   })
 }
-const getUserContacts = (req, res) => {
-  let sql = "SELECT * FROM userscontact"
-  sql = mysql.format(sql, [ req.params.id ])
-  pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(results);
-  })
-}
- 
-// const createUserId = (req, res) => {
-//   let newId = id.length + 1;
-//   let userId = {
-//     _id: id,
-//     body: req.body.body,
-//     postId: 1,
-// };
-// newId.push(userId);
-// res.json(newId);
-// };
 
-const createUser = (req, res) => {
-  const { userId, firstName, lastName } = req.body
-  let sql = "INSERT INTO users (user_id, first_name, last_name) VALUES (?,  ?, ?)"
-  sql = mysql.format(sql, [userId, firstName, lastName] )
+ const createUser = (req, res) => {
+  const {firstName, lastName, email, phone, city, state, zip} = req.body
+  let sql = "INSERT INTO users (first_name, last_name, email, phone, city, state, zip) VALUES ('??', '??', '??', '??', '??', '??', '??')"
+  sql = mysql.format(sql, [firstName, lastName, email, phone, city, state, zip] )
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -49,80 +30,31 @@ const createUser = (req, res) => {
   })
 }
 
+const updateUserById = (req, res) => {
+  const { firstName, lastName, email, phone, city, state, zip} = req.body
+  let sql = "UPDATE users SET first_name = '?', last_name = '?', email = '?', phone = '?', city = '?', state ='?', zip = '?' WHERE id = ?"
+  sql = mysql.format(sql, [ firstName, lastName, email, phone, city, state, zip, req.params.id ])
 
-// const createUserContact = (req, res) => {
-//   const { userId, email, phone } = req.body
-//   let sql = "INSERT INTO userscontact (user_id, email, phone) VALUES (?, ?, ?)"
-//   sql = mysql.format(sql, [ userId, email, phone ])
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.status(204).json();
+  })
+}
 
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json(rows);
-//   })
-// }
+const deleteUserById = (req, res) => {
+  let sql = "DELETE FROM users WHERE id = ?"
+  sql = mysql.format(sql, [ req.params.id ])
 
-// const createUserLocation = (req, res) => {
-//   const { city, state, zip } = req.body
-//   let sql = "INSERT INTO userslocation (city, state, zip) VALUES (?, ?, ?)"
-//   sql = mysql.format(sql, [ firstName, lastName ])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json({ newId: results.insertId });
-//   })
-// }
-
-// const updateUserById = (req, res) => {
-//   const { firstName, lastName } = req.body
-//   let sql = "UPDATE users SET first_name = ?, last_name = ? WHERE id = ?"
-//   sql = mysql.format(sql, [ firstName, lastName, req.params.id ])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.status(204).json();
-//   })
-// }
-// const updateUserContact = (req, res) => {
-//   const { email, phone } = req.body
-//   let sql = "UPDATE users SET email = ?, phone = ? WHERE id = ?"
-//   sql = mysql.format(sql, [ email, phone, req.params.id ])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.status(204).json();
-//   })
-// }
-
-// const updateUserLocation = (req, res) => {
-//   const { city, state, zip } = req.body
-//   let sql = "UPDATE users SET city = ?, state = ? zip = ? WHERE id = ?"
-//   sql = mysql.format(sql, [ city, state, zip, req.params.id ])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.status(204).json();
-//   })
-// }
-
-// const deleteUserById = (req, res) => {
-//   let sql = "DELETE FROM users WHERE id = ?"
-//   sql = mysql2.format(sql, [ req.params.first_name ])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
-//   })
-// }
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+  })
+}
 
 module.exports = {
   getAllUsers,
   getUserById,
-  getUserContacts,
-  createUser
-  // createUserContact
-  // createUserLocation,
-  // updateUserById,
-  // updateUserContact,
-  // updateUserLocation,
-  // deleteUserById
+  createUser,
+  updateUserById,
+  deleteUserById
 }
