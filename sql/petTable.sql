c948f35f699672d2ef6351e5f4d670985df5e037
RosalynS78 <rrcervania78@gmail.com>
2022-06-05 10:47:04 -0500

    started pet table

--- sql/petTable.sql
+++ sql/petTable.sql
@@ -1,41 +1,50 @@
-DROP TABLE IF EXISTS pet, pet_type, pet_status, pet_description, pet_location;
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS pet_found, pet_missing, pet_type, pet_location;
 
-pet Table:
-	CREATE TABLE pet (
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		pet_id INT NOT NULL,
-		FOREIGN KEY (users_id) REFERENCES users (id),
-		pet_name VARCHAR (100) 
-);
-
-pet_type Table:
-	CREATE TABLE pet_type (
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		pet_id INT NOT NULL,
-		pet_type VARCHAR (100) NOT NULL
+CREATE TABLE pet_found (
+	pet_id INT NOT NULL AUTO_INCREMENT,
+	pet_name VARCHAR (50) NOT NULL,
+	sex VARCHAR (10) NOT NULL,
+	age VARCHAR (10) NOT NULL,
+	photo_LONGBLOB NOT NULL,
+    lastseen_date VARCHAR (50),
+	PRIMARY KEY (pet_id)
+	FOREIGN KEY (id) REFERENCES users (id) 
+	ON DELETE CASCADE
 );
 
-pet_status Table:
-	CREATE TABLE pet_status (
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		pet_id INT NOT NULL,
-		tag VARCHAR (50) NOT NULL,
-        lastseen_date  VARCHAR (50)
+CREATE TABLE pet_missing (
+	pet_id INT NOT NULL AUTO_INCREMENT,
+	pet_name VARCHAR (50) NOT NULL,
+	sex VARCHAR (10) NOT NULL,
+	age VARCHAR (10) NOT NULL,
+	photo_LONGBLOB NOT NULL,
+    lastseen_date VARCHAR (50),
+	PRIMARY KEY (pet_id)
+	FOREIGN KEY (id) REFERENCES users (id) 
+	ON DELETE CASCADE
 );
 
-pet_description Table:
-	CREATE TABLE pet_description (
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		pet_id INT NOT NULL,
-		sex VARCHAR (50) NOT NULL,
-        age  VARCHAR (50),
-        breed_date VARCHAR (50),
-        photo
+CREATE TABLE pet_type (
+	pet_id INT NOT NULL AUTO_INCREMENT,
+	dog,
+	cat,
+	bird,
+	reptile,
+	other
 );
-pet_location Table:
-	CREATE TABLE pet_location (
-		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		pet_id INT NOT NULL,
-		latitude,
-        longitude
+
+CREATE TABLE pet_location (
+	pet_id INT NOT NULL AUTO_INCREMENT,
+	latitude,
+    longitude
 );
+
+INSERT INTO users
+	(first_name, last_name)
+VALUES
+	("Joe", "Dirt"),
+	("Ricky", "Martin"),
+	("Celine", "Dion"),
+	("Sandra", "Bullock"),
+	("Channing", "Tatum");
